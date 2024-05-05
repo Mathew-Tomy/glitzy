35abcacfdb02ac7635f4f427e1ef12cdebd20819
Mathew Tomy <159645212+Mathew-Tomy@users.noreply.github.com>
2024-05-04 21:33:13 -0400

    Update footer_widget.dart

--- lib/widgets/footer_widget.dart
+++ lib/widgets/footer_widget.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:glitzy/config/theme.dart';
 
+import 'package:glitzy/constants.dart';
+const Color inActiveIconColor = Color(0xFFB6B6B6);
 class Footerwidget extends StatefulWidget {
+
   const Footerwidget({Key? key}) : super(key: key);
 
   @override
@@ -9,21 +14,55 @@ class Footerwidget extends StatefulWidget {
 }
 
 class _FooterwidgetState extends State<Footerwidget> {
+  final List<String> routeNames = ['/dashboard', '/cart', '/wishlist', '/order']; // Define your route names
+  int currentSelectedIndex = 0;
+
+  void updateCurrentIndex(int index) {
+    setState(() {
+      currentSelectedIndex = index;
+    });
+  }
+
+
+
   @override
+
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black,
+
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Text('Copyright © 2020 Dsouq. All Rights Reserved',style: TextStyle(color: Colors.white),),
-            SizedBox(height: 5,),
-            Text('Powered by SUNUNITED INFOLOGICS.',style: TextStyle(color: Colors.white))
+            _buildIconButton(Icons.home, 'Home', () => Navigator.pushNamed(context, '/dashboard')),
+            _buildIconButton(Icons.shopping_cart, 'Cart', () => Navigator.pushNamed(context, '/cart')),
+            _buildIconButton(Icons.favorite, 'Wishlist', () => Navigator.pushNamed(context, '/wishlist')),
+            _buildIconButton(Icons.assignment, 'Orders', () => Navigator.pushNamed(context, '/order')),
           ],
         ),
       ),
     );
   }
+
+  Widget _buildIconButton(IconData iconData, String label, Function() onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Icon(
+            iconData,
+            color: Colors.orange,
+            size: 30,
+          ),
+          SizedBox(height: 5),
+          // Text(
+          //   label,
+          //   style: TextStyle(color: Colors.orange),
+          // ),
+        ],
+      ),
+    );
+  }
 }
+
